1cc8ac2db2e72d5beafdc7a9d1a5400b0060f875
Audrey Alaime <audreyalaime3@gmail.com>
2023-07-04 11:56:49 +0200

    Mis en place de l'horloge avec un useEffect équivalent à componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import {getElapsedTime, startTime, startHms, getHms, getTickerStep, addMissing0, safeCheckTicker, hasBeenForced } from './_';
+import React, { useEffect, useState } from 'react';
+import {
+  getElapsedTime,
+  startTime,
+  startHms,
+  getHms,
+  getTickerStep,
+  addMissing0,
+  safeCheckTicker,
+  hasBeenForced,
+} from './_';
 
-import './App.css'
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const [clock, setClock] = useState(startTime);
   const [tickerStep, setTickerStep] = useState(clock.getSeconds() % 2 ? 'Tic' : 'Tac');
-  
+
   const hms = getHms(clock);
   const spentHms = getElapsedTime(count);
 
@@ -15,65 +24,59 @@ function App() {
     const d = new Date();
     setTickerStep(getTickerStep(d));
     setClock(d);
-  }
-  
-  useEffect(tick, [count])
-  
-  useEffect(() => {
-    if (hasBeenForced()) {
-      alert("Something is really wrong with this component and we had to force reload :(\n We'll refresh the page to bring you at starting point");
-      document.location = "/"
-      return;
-    }
+  };
 
+  useEffect(() => { // Code à exécuter lors du montage du composant (équivalent à componentDidMount)
     const ticker = setInterval(() => {
       if (!safeCheckTicker(ticker)) return;
 
-      const newCount = count + 1;
-      
-      console.log("setCount with new value: %s", newCount)
-      
-      setCount(newCount);
-    
+      setCount((prevCount) => prevCount + 1);
     }, 1_000);
-    
-    console.log("Ticker id is: %s", ticker);
 
-  }, [])
+    const forcedReload = hasBeenForced();
+    if (forcedReload) {
+      alert("Something is really wrong with this component and we had to force reload :(\n We'll refresh the page to bring you to the starting point");
+      document.location = "/";
+    }
+
+    return () => {
+      clearInterval(ticker);  // Code de nettoyage à exécuter lors du démontage du composant (équivalent à componentWillUnmount)
+    };
+  }, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    tick();
+  }, [count]);
 
   return (
     <div className="App">
-      <span>
-        {tickerStep}
-      </span>
+      <span>{tickerStep}</span>
 
       <span>
         Started at:
-        {" "}
-        {startHms.map(addMissing0).join(":")}
+        {' '}
+        {startHms.map(addMissing0).join(':')}
       </span>
 
       <span>
         Current time is:
-        <br /> 
-        {hms.map(addMissing0).join(":")}
+        <br />
+        {hms.map(addMissing0).join(':')}
       </span>
 
       <span>
         Spent time:
-        <br /> 
-        {spentHms.map(addMissing0).join(":")}
+        <br />
+        {spentHms.map(addMissing0).join(':')}
       </span>
 
       <span>
         Count:
-        <br /> 
+        <br />
         {`${count}`}
       </span>
-
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
